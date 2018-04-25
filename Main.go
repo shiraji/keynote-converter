564c67c7e4d06374274ffa3154762ac8ffcec0cb
shiraji <isogai.shiraji@gmail.com>
2018-04-25 23:57:35 +0900

    Add more files

---
+++ Main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os/exec"
+	"fmt"
+	"strings"
+	"flag"
+)
+
+func main() {
+
+	dir := flag.Arg(0)
+
+	fmt.Println(dir)
+
+	out, _ := exec.Command("find",
+		dir,
+		"-not", "-name", "*.pdf*",
+		"-not", "-name", "*.json*",
+		"-not", "-name", "*.jpeg",
+		"-not", "-name", "*.png",
+		"-not", "-name", "*.js",
+		"-not", "-name", "*.bcmap",
+		"-not", "-name", "*.DS_Store",
+		"-not", "-name", "*.html",
+		"-not", "-name", "*.css",
+		"-not", "-name", "LICENSE",
+		"-type", "f",
+	).Output()
+
+	results := strings.Split(string(out), "\n")
+
+	for _, value := range results {
+		fmt.Println("-----")
+		fmt.Println(value)
+	}
+}
